6f2ed0351cbdcd6efeaeebf35a7c6570a3c65507
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2023-02-01 12:03:19 +0100

    Started to implement app till uber mesed up

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+
+mod coords;
+mod drivers;
+mod rides;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about)]
+struct Commands {
+    #[arg(short, long)]
+    book: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut vector: Vec<String> = Vec::new();
+    // drivers::view_drivers();
+    let current_coords = coords::get_coords(String::from("296a jide oki")).ok().unwrap();
+
+    vector.push(current_coords[0].clone());
+    vector.push(current_coords[1].clone());
+
+    rides::get_ride_eta(vector);
 }
